bb0e4bb268264be80c6f378c56b487ba84201f74
zengdexun <vp00792@globalymc.com>
2021-12-03 18:41:55 +0800

    feat: menu新增

---
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<template>
+			<el-menu-item index="2">
+				<i class="el-icon-menu"></i>
+				<span slot="title">导航二</span>
+			</el-menu-item>
+		</template>
+	
+		<template>
+			<el-submenu index="1">
+				<template slot="title">
+					<i class="el-icon-location"></i>
+					<span slot="title">导航一</span>
+				</template>
+				<el-menu-item index="1-1">选项1</el-menu-item>
+				<el-menu-item index="1-2">选项2</el-menu-item>
+			</el-submenu>
+		</template>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -3,24 +3,11 @@
 		<el-scrollbar wrap-class="scrollbar-wrapper">
 			<el-menu
 				default-active="2"
-				@open="handleOpen"
-				@close="handleClose"
 				:background-color="variables.menuBg"
 				:text-color="variables.menuText"
 				:active-text-color="variables.menuActiveText"
 			>
-				<el-menu-item index="2">
-					<i class="el-icon-menu"></i>
-					<span slot="title">导航二</span>
-				</el-menu-item>
-				<el-menu-item index="3" disabled>
-					<i class="el-icon-document"></i>
-					<span slot="title">导航三</span>
-				</el-menu-item>
-				<el-menu-item index="4">
-					<i class="el-icon-setting"></i>
-					<span slot="title">导航四</span>
-				</el-menu-item>
+				<sidebar-item></sidebar-item>
 			</el-menu>
 		</el-scrollbar>
 	</el-aside>
@@ -28,7 +15,12 @@
 
 <script>
 import variables from "@/styles/variables.scss";
+import SidebarItem from './SidebarItem.vue';
+
 export default {
+	components: {
+		SidebarItem,
+	},
 	computed: {
 		variables() {
 			return variables;
